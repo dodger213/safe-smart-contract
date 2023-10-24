8f5976bf97a6592f6a1325fe8ae0b886525437e5
dodger213 <mitsurukudo213@gmail.com>
2023-10-24 17:23:55 +0200

    WIP Update from 1.1.1

--- contracts/libraries/SafeToL2Migration.sol
+++ contracts/libraries/SafeToL2Migration.sol
@@ -8,6 +8,12 @@ import {Enum} from "../common/Enum.sol";
 interface ISafe {
     // solhint-disable-next-line
     function VERSION() external view returns (string memory);
+
+    function setFallbackHandler(address handler) external;
+
+    function getOwners() external view returns (address[] memory);
+
+    function getThreshold() external view returns (uint256);
 }
 
 /**
@@ -34,6 +40,8 @@ contract SafeToL2Migration is SafeStorage {
      */
     event ChangedMasterCopy(address singleton);
 
+    event SafeSetup(address indexed initiator, address[] owners, uint256 threshold, address initializer, address fallbackHandler);
+
     event SafeMultiSigTransaction(
         address to,
         uint256 value,
@@ -50,27 +58,7 @@ contract SafeToL2Migration is SafeStorage {
         bytes additionalInfo
     );
 
-    /**
-     * @notice Migrate from Safe 1.3.0/1.4.1 Singleton (L1) to the same version provided L2 singleton
-     * Safe is required to have nonce 0 so backend can support it after the migration
-     * @dev This function should only be called via a delegatecall to perform the upgrade.
-     * Singletons versions will be compared, so it implies that contracts exist
-     */
-    function migrateToL2(address l2Singleton) public {
-        require(address(this) != MIGRATION_SINGLETON, "Migration should only be called via delegatecall");
-        require(address(singleton) != l2Singleton, "Safe is already using the singleton");
-        // Nonce is increased before executing a tx, so first executed tx will have nonce=1
-        require(nonce == 1, "Safe must have not executed any tx");
-        bytes32 oldSingletonVersion = keccak256(abi.encodePacked(ISafe(singleton).VERSION()));
-        bytes32 newSingletonVersion = keccak256(abi.encodePacked(ISafe(l2Singleton).VERSION()));
-
-        require(oldSingletonVersion == newSingletonVersion, "L2 singleton must match current version singleton");
-        // There's no way to make sure if address is a valid singleton, unless we cofigure the contract for every chain
-        require(
-            newSingletonVersion == keccak256(abi.encodePacked("1.3.0")) || newSingletonVersion == keccak256(abi.encodePacked("1.4.1")),
-            "Provided singleton version is not supported"
-        );
-
+    function migrate(address l2Singleton) private {
         singleton = l2Singleton;
 
         // Simulate a L2 transaction so indexer picks up the Safe
@@ -92,5 +80,68 @@ contract SafeToL2Migration is SafeStorage {
             additionalInfo
         );
         emit ChangedMasterCopy(singleton);
+    }
+
+    /**
+     * @notice Migrate from Safe 1.3.0/1.4.1 Singleton (L1) to the same version provided L2 singleton
+     * Safe is required to have nonce 0 so backend can support it after the migration
+     * @dev This function should only be called via a delegatecall to perform the upgrade.
+     * Singletons versions will be compared, so it implies that contracts exist
+     */
+    function migrateToL2(address l2Singleton) public {
+        require(address(this) != MIGRATION_SINGLETON, "Migration should only be called via delegatecall");
+
+        require(address(singleton) != l2Singleton, "Safe is already using the singleton");
+        // Nonce is increased before executing a tx, so first executed tx will have nonce=1
+        require(nonce == 1, "Safe must have not executed any tx");
+        bytes32 oldSingletonVersion = keccak256(abi.encodePacked(ISafe(singleton).VERSION()));
+        bytes32 newSingletonVersion = keccak256(abi.encodePacked(ISafe(l2Singleton).VERSION()));
+
+        require(oldSingletonVersion == newSingletonVersion, "L2 singleton must match current version singleton");
+        // There's no way to make sure if address is a valid singleton, unless we cofigure the contract for every chain
+        require(
+            newSingletonVersion == keccak256(abi.encodePacked("1.3.0")) || newSingletonVersion == keccak256(abi.encodePacked("1.4.1")),
+            "Provided singleton version is not supported"
+        );
+
+        migrate(l2Singleton);
+    }
+
+    function migrateFromV111(address l2Singleton, address fallbackHandler) public {
+        require(address(this) != MIGRATION_SINGLETON, "Migration should only be called via delegatecall");
+
+        bytes32 oldSingletonVersion = keccak256(abi.encodePacked(ISafe(singleton).VERSION()));
+        require(oldSingletonVersion == keccak256(abi.encodePacked("1.1.1")), "Provided singleton version is not supported");
+
+        bytes32 newSingletonVersion = keccak256(abi.encodePacked(ISafe(l2Singleton).VERSION()));
+        require(
+            newSingletonVersion == keccak256(abi.encodePacked("1.3.0")) || newSingletonVersion == keccak256(abi.encodePacked("1.4.1")),
+            "Provided singleton version is not supported"
+        );
+
+        require(isContract(fallbackHandler), "fallbackHandler is not a contract");
+        ISafe safe = ISafe(address(this));
+        safe.setFallbackHandler(fallbackHandler);
+        emit SafeSetup(MIGRATION_SINGLETON, safe.getOwners(), safe.getThreshold(), address(0), fallbackHandler);
+        migrate(l2Singleton);
+    }
+
+    /**
+     * @notice Checks whether an Ethereum address corresponds to a contract or an externally owned account (EOA).
+     * @param account The Ethereum address to be checked.
+     * @return A boolean value indicating whether the address is associated with a contract (true) or an EOA (false).
+     * @dev This function relies on the `extcodesize` assembly opcode to determine whether an address is a contract.
+     * It may return incorrect results in some edge cases (see documentation for details).
+     * Developers should use caution when relying on the results of this function for critical decision-making.
+     */
+    function isContract(address account) internal view returns (bool) {
+        uint256 size;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            size := extcodesize(account)
+        }
+
+        // If the code size is greater than 0, it is a contract; otherwise, it is an EOA.
+        return size > 0;
     }
 }
